162eace33881e48b53f259786087f4d0bab9ca85
Tagliapietra96 <tagliatek96@gmail.com>
2022-11-03 15:29:48 +0100

    set variables

---
+++ js/dadi.js
@@ -0,0 +1,12 @@
+const startContainerEl = document.querySelector('body div:first-child');
+const endContainerEl = document.querySelector('body div:last-child')
+const startBtnEl = document.getElementById('start-btn');
+const answerEl = document.querySelector('h1');
+const userOutputEl = document.getElementById('you');
+const pcOutputEl = document.getElementById('computer');
+const endBtnEl = document.getElementById('end-btn');
+
+let userInput;
+let pcInput;
+
+
